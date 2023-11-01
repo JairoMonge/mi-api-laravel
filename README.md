ba15b7778cef501169735fcc04a4e5751a505f4e
JairoMonge <jairo@makeitsimple.io>
2023-11-01 17:06:01 -0600

    Se cambió la ruta de web.php a api.php

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ composer install
 2-luego echamos a andar el sistema con 
 
 "php artisan serve"
+
+Nota: Antes de correr Laravel cambiar la ruta del servicio que consumirá la API en app/Http/Middleware/Cors.php
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MensajeController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/enviar-mensaje', [MensajeController::class, 'enviarMensaje']);
+
 
